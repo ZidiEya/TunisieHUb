31d2765df374cfec800235b792d186e98a587d2b
Eya Zidi <3805@holbertonschool.com>
2025-09-25 19:17:19 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 // Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
+ import { supabase } from "@/integrations/supabase/client";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
